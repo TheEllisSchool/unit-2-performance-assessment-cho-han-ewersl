b8306518413803d74a23a75c7bea4c91fcfcf45f
leahewers <leahewers@leahs-air.fios-router.home>
2018-01-18 00:00:05 -0500

    leah's updated loaded die that still doesnt work lol

--- src/LoadedDice.java
+++ src/LoadedDice.java
@@ -9,14 +9,15 @@ public class LoadedDice extends Dice {
 	public int generateRoll () {
 	int firstNum = (int) (Math.random () * 100) +1;
 	if (firstNum <= 80) { 
-		int allNumRoll = (int) (Math.random () *100) +1;
-		int roll = allNumRoll *2; 
-		else {
-			
-		}
+		int allNumRoll = (int) (Math.random () *getnumSides()) +1;
+		int roll = allNumRoll *2 - 1; 
 		return roll;
 		}
-	} 
+	else { 
+		int allNumRoll = (int) (Math.random()* getnumSides()) +1;
+		int roll = allNumRoll *2;
+		return roll;
+	}
 
-
+	}
 }
